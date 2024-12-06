669a08f28e1f22d62857e4bdc17fc391f8666ac8
johndlr <143981556+johndlr@users.noreply.github.com>
2024-12-06 17:53:33 -0600

    Add two new Java entities and the required dependencies for JWT

---
+++ src/main/java/com/juandlr/jwtproject/entity/ApplicationUser.java
@@ -0,0 +1,37 @@
+package com.juandlr.jwtproject.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "users")
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+public class ApplicationUser {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private long userId;
+
+    @Column(name = "user_name")
+    private String userName;
+
+
+    private String name;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String email;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    @ManyToOne()
+    @JoinColumn(name = "role_id")
+    private Role role;
+}
